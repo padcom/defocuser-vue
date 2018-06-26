dd037fca776a44d845b5d54047b24934c62d143b
Matthias Hryniszak <padcom@gmail.com>
2018-06-27 00:00:44 +0200

    Updated rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,19 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-
 import minify from 'rollup-plugin-minify'
-
-const pkg = require('./package.json')
-const external = Object.keys(pkg.dependencies)
 
 export default {
   input: 'index.js',
-  external,
   output: [ {
-    globals: [ 'defocuser' ],
     file: 'dist/defocuser-vue.umd.js',
     format: 'umd',
     name: 'defocuser-vue',
   } ],
   plugins: [
-    babel({ exclude: 'node_modules/**' }),
     resolve(),
     commonjs(),
+    babel({ exclude: 'node_modules/**' }),
     minify({ umd: 'dist/defocuser-vue.umd.min.js' })
   ]
 }
